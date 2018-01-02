6237cfbf1e86821d4949b0d1e71403cd6c69ba4f
Domagoj Krivošić <domagoj_70@hotmail.com>
2018-01-02 15:43:13 +0100

    Add point class which represents a point in space

---
+++ src/Point.java
@@ -0,0 +1,37 @@
+
+public class Point {
+	private double x;
+	private double y;
+	
+	public Point(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	@Override
+	public String toString() {
+		return "(" + x + ", " + y + ")";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Point) {
+			return false;
+		}
+		
+		double e = 1e-6;
+		Point p = (Point) obj;
+		
+		return Math.abs(p.x - x) < e && Math.abs(p.y - y) < e;
+	}
+
+}

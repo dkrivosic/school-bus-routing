ad7582b4d7330af8f52933608e62ec4e7b539b85
Domagoj Krivošić <domagoj_70@hotmail.com>
2018-01-03 20:08:22 +0100

    Add random number generator

---
+++ src/RandomNumberGenerator.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class RandomNumberGenerator {
+	private Random random;
+	
+	public RandomNumberGenerator() {
+		this.random = new Random();
+	}
+	
+	public double nextDouble() {
+		return random.nextDouble();
+	}
+	
+	public int nextInt(int upperBound) {
+		return random.nextInt(upperBound);
+	}
+	
+	public int nextInt(int lowerBound, int upperBound) {
+		return random.nextInt(upperBound - lowerBound) + lowerBound;
+	}
+	
+
+}

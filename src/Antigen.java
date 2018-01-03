422bad5076c292701184ae34cf0b393541bd75f0
Domagoj Krivošić <domagoj_70@hotmail.com>
2018-01-03 20:35:57 +0100

    Add antigen

---
+++ src/Antigen.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+public class Antigen {
+	private ProblemInstance problem;
+
+	public Antigen(ProblemInstance problem) {
+		super();
+		this.problem = problem;
+	}
+	
+	public void evaluate(Antibody a) {
+		double cost = 0.0;
+		int school = 0;
+		
+		for (int bus = 0; bus < a.busRoutes.size(); bus++) {
+			List<Integer> route = a.busRoutes.get(bus);
+			int currentStop = route.get(0);
+			cost += problem.getStopsDistance(school, currentStop);
+			int lastStop = currentStop;
+			for (int i = 1; i < route.size(); i++) {
+				lastStop = currentStop;
+				currentStop = route.get(i);
+				cost += problem.getStopsDistance(lastStop, currentStop);
+			}
+			cost += problem.getStopsDistance(currentStop, school);
+		}
+		a.cost = cost;
+	}
+	
+	public void evaluate(List<Antibody> population) {
+		for (Antibody a : population) {
+			evaluate(a);
+		}
+	}
+}

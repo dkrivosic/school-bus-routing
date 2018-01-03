8ce0459d7a16ae6fee7d31de7339631e40215ea8
Domagoj Krivošić <domagoj_70@hotmail.com>
2018-01-03 17:37:36 +0100

    Add the program entry point

---
+++ src/Runner.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Runner {
+	public static void main(String[] args) throws IOException {
+		if (args.length < 1) {
+			System.err.println("Expected 1 argument: path to the file with problem description");
+			System.exit(1);
+		}
+		
+		ProblemInstance.readFromFile(args[0]);
+	}
+}

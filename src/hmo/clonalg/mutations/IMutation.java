4633c037de547c3129b18ceb44dd37d01ce154e1
Domagoj Krivošić <domagoj_70@hotmail.com>
2018-01-04 15:46:30 +0100

    Change IMutation to abstract class, add method for mutating population

--- src/hmo/clonalg/mutations/IMutation.java
+++ src/hmo/clonalg/mutations/IMutation.java
@@ -1,7 +1,16 @@
 package hmo.clonalg.mutations;
 
+import java.util.List;
+
 import hmo.clonalg.Antibody;
 
-public interface IMutation {
-	void mutate(Antibody a);
+public abstract class IMutation {
+	
+	public abstract void mutate(Antibody a);
+	
+	public void mutate(List<Antibody> population) {
+		for (Antibody a : population) {
+			this.mutate(a);
+		}
+	}
 }

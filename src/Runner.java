96204af243f2bec5ab518cf348854e378ed30e5a
Domagoj Krivošić <domagoj_70@hotmail.com>
2018-01-04 17:38:40 +0100

    Initialize and run algorithm in main

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,16 @@
 import java.io.IOException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import hmo.clonalg.Antibody;
+import hmo.clonalg.Antigen;
+import hmo.clonalg.CloneOperator;
 import hmo.clonalg.ProblemInstance;
+import hmo.clonalg.algorithm.Clonalg;
+import hmo.clonalg.mutations.ChangeStudentStationMutation;
+import hmo.clonalg.mutations.Hypermutation;
+import hmo.clonalg.mutations.IMutation;
+import hmo.clonalg.utils.RandomNumberGenerator;
 
 public class Runner {
 	public static void main(String[] args) throws IOException {
@@ -8,7 +18,23 @@ public class Runner {
 			System.err.println("Expected 1 argument: path to the file with problem description");
 			System.exit(1);
 		}
+
+		ProblemInstance problem = ProblemInstance.readFromFile(args[0]);
+		RandomNumberGenerator random = new RandomNumberGenerator();
+		Antigen antigen = new Antigen(problem);
+		CloneOperator cloneOperator = new CloneOperator(0.5);
+		int populationSize = 20;
+		int maxIterations = 10;
+		int generateNew = 2;
+
+		IMutation hypermutation = new Hypermutation(
+				Stream.of(
+						new ChangeStudentStationMutation(0.1, random, problem)
+				).collect(Collectors.toList()));
 		
-		ProblemInstance.readFromFile(args[0]);
+		Clonalg algorithm = new Clonalg(populationSize, maxIterations, generateNew, problem, random, antigen, cloneOperator, hypermutation);
+		
+		Antibody result = algorithm.run();
+		System.out.println("Cost: " + result.cost);
 	}
 }

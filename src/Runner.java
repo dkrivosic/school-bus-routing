adb90518cc2774f0b016b68c5c6ce3855381edd4
Domagoj Krivošić <domagoj_70@hotmail.com>
2018-01-18 08:55:44 +0100

    Enable Running multiple problem instances in a row

--- src/Runner.java
+++ src/Runner.java
@@ -18,29 +18,33 @@ import hmo.clonalg.utils.ResultFileWriter;
 public class Runner {
 	public static void main(String[] args) throws IOException {
 		if (args.length < 1) {
-			System.err.println("Expected 1 argument: path to the file with problem description");
+			System.err.println("At least 1 argument expected: path to the file with problem description");
 			System.exit(1);
 		}
-
-		ProblemInstance problem = ProblemInstance.readFromFile(args[0]);
-		ResultFileWriter writer = new ResultFileWriter(args[0]);
-		RandomNumberGenerator random = new RandomNumberGenerator();
-		Antigen antigen = new Antigen(problem);
-		CloneOperator cloneOperator = new CloneOperator(0.5);
-		int populationSize = 50;
-		int maxIterations = 1000;
-		int generateNew = 2;
-
-		IMutation hypermutation = new Hypermutation(Stream.of(new ChangeStudentStationMutation(0.05, random, problem),
-				new RemoveBusMutation(0.05, random, problem),
-				new SwapStationsMutation(0.1, random)).collect(Collectors.toList()));
-
-		Clonalg algorithm = new Clonalg(populationSize, maxIterations, generateNew, problem, random, antigen,
-				cloneOperator, hypermutation, writer);
-
-		Antibody result = algorithm.run();
-		System.out.println("Cost: " + result.cost);
-		writer.write(result, "ne");
-		System.out.println("Total number of evaluations: " + antigen.getNumberOfEvaluations());
+		
+		for (String filename : args) {
+			System.out.println("Problem " + filename);
+			ProblemInstance problem = ProblemInstance.readFromFile(filename);
+			ResultFileWriter writer = new ResultFileWriter(filename);
+			RandomNumberGenerator random = new RandomNumberGenerator();
+			Antigen antigen = new Antigen(problem);
+			CloneOperator cloneOperator = new CloneOperator(0.5);
+			int populationSize = 100;
+			int maxIterations = 150000;
+			int generateNew = 5;
+	
+			IMutation hypermutation = new Hypermutation(Stream.of(new ChangeStudentStationMutation(0.05, random, problem),
+					new RemoveBusMutation(0.05, random, problem),
+					new SwapStationsMutation(0.1, random)).collect(Collectors.toList()));
+	
+			Clonalg algorithm = new Clonalg(populationSize, maxIterations, generateNew, problem, random, antigen,
+					cloneOperator, hypermutation, writer);
+	
+			Antibody result = algorithm.run();
+			System.out.println("Cost: " + result.cost);
+			writer.write(result, "ne");
+			System.out.println("Total number of evaluations: " + antigen.getNumberOfEvaluations());
+			System.out.println();
+		}
 	}
 }
